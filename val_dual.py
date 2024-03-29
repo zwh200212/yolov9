8a91c6603354d0ae248b849a3676b3eeaccf4355
zwh200212 <18834669468@163.com>
2024-03-29 23:56:20 +0800

    尝试修正val loss

--- val_dual.py
+++ val_dual.py
@@ -195,8 +195,8 @@ def run(
         # Loss
         if compute_loss:
             preds = preds[1]
-            #train_out = train_out[1]
-            #loss += compute_loss(train_out, targets)[1]  # box, obj, cls
+            train_out = train_out[1]
+            loss += compute_loss(train_out, targets)[1]  # box, obj, cls
         else:
             preds = preds[0][1]
 
